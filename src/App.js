bbc3961d37fe8951fa07038ca206a4cacede6eb5
imt-eiaz <imteiaz@gmail.com>
2025-09-08 18:31:43 +0100

    9. Calculating Statistics as Derived State

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
           onDeleteItem={handleDeleteItem}
           onToggleItem={handleToggleItem}
         />
-        <Stats />
+        <Stats items={items} />
       </div>
     </>
   );
@@ -49,8 +49,6 @@ function Form({ onAddItems }) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-
-    console.log(newItem);
 
     onAddItems(newItem);
     setDescription("");
@@ -114,10 +112,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em> 👜 You have X items on your list, and you alraedy packed x (x%)</em>;
+      <em>
+        {percentage === 100
+          ? "You got everything! ready to go ✈"
+          : `👜 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
