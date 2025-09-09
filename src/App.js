8ea02f4ef09854c4d9e4f6b2005b1fe6f1f76c18
imt-eiaz <imteiaz@gmail.com>
2025-09-09 18:51:03 +0100

    11. Clearing the List

--- src/App.js
+++ src/App.js
@@ -19,6 +19,15 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items? "
+    );
+    if (confirmed) {
+      setItems([]);
+    }
+  }
+
   return (
     <>
       <div className="app">
@@ -28,6 +37,7 @@ export default function App() {
           items={items}
           onDeleteItem={handleDeleteItem}
           onToggleItem={handleToggleItem}
+          onClearList={handleClearList}
         />
         <Stats items={items} />
       </div>
@@ -79,7 +89,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState("input");
 
   let sortedItems;
@@ -114,6 +124,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status </option>
         </select>
+        <button onClick={onClearList}>Clear list</button>
       </div>
     </div>
   );
